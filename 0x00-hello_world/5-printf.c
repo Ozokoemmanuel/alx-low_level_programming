473fe9efd9111e1939b772095fc6c372f041abbc
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-01 16:17:50 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }

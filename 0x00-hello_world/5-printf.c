c40301f6c3e40c3b8f0783ab89460393480d0952
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-01 16:36:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/*int main is the entry part of the program*/
 
 int main(void)
-/*int main is the entry part of the program*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-         /*return: always 0 (success)*/
+        
 }	

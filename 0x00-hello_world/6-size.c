da9e452635f8c77550bdd8d1d4bf053adb99204d
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-01 17:06:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu.\n", (unsigned long)sizeof(g));
 printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(h));
+printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
 
 return(0);
 }

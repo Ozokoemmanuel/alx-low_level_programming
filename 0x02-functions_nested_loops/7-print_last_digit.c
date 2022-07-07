c882037fcfc6db4ed6c933c599255f6e81e6c8cf
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-07 10:42:22 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+			_putchar ((n % 10) + '0');
+		return (n % 10);
+}

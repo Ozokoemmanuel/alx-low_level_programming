26c9dfa500b551f83ec2e8ddbc3f92db05f00789
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-07 16:14:52 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main -Entry
- *
+ *  _abs - calculate the absolute value of the number from zero.
+ * @i: the integer to be converted.
  * Return: Always 0
  */
 int _abs(int i)

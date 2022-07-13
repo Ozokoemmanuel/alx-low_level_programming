707962c77c8ecb2c42b8693e86d2db9a14e89d6b
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-13 10:00:59 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
-
-
 /**
  *
  *  * main - check the code
@@ -26,19 +23,4 @@ int main(void)
 		    puts_half(str);
 
 		        return (0);
-
-=======
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *str;
-
-    str = "0123456789";
-    puts_half(str);
-    return (0);
->>>>>>> e8cd01f9cf03a9a629b272087d46dcd1f97541ca
 }

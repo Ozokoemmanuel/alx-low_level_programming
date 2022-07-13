e92531ad7b83802c425216ed17fd954e92a756eb
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-13 09:12:46 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 
 
 /**
@@ -27,18 +26,4 @@ int main(void)
 
 		        return (0);
 
-=======
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
->>>>>>> e8cd01f9cf03a9a629b272087d46dcd1f97541ca
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 /**
  * print_rev - prints reversed string, followed by a new line
  * @s: pointer to the string to print

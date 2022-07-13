e4a7dece735039e190b083f0f7fcb82459d5369d
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-13 09:35:10 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,22 @@
-#include "main.c"
+#include "main.h"
 /**
- * rev_string - prints reversed string, followed by a new line
- * @s: pointer to the string to print
- * Return: void
-*/
-
+ * rev_string - Reverses a string
+ * @s: pointer to the string to reverse
+ * Return
+ */
 void rev_string(char *s)
 {
+	int len, i, half;
+	char temp;
 
-int len, i, half;
-char temp;
-
-for (len = 0; s[len] != '\0'; len++)
-;
-
-i = 0;
-
-half = len / 2;
-
-while (half--)
-{
-	temp = s[len - i - 1];
-	s[len - i - 1] = s[i];
-	s[i] = temp;
-	i++;
-}
-
+	for (len = 0; s[len] != '\0'; len++);
+	i = 0;
+	half = len / 2;
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
 }

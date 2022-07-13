1ce83a3f404f5d9303c494bd22385fd274511a21
Ozokoemmanuel <pcozoko@gmail.com>
2022-07-13 09:41:31 -0700

    9-strcpy

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,6 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
 #include <stdio.h>
 
 
@@ -32,23 +30,4 @@ int main(void)
 			    printf("%s", ptr);
 
 			        return (0);
-
-=======
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
->>>>>>> e8cd01f9cf03a9a629b272087d46dcd1f97541ca
 }
